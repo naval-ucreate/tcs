e9611a762f61108bd464bf5f22ad3871bb740c11
ucreate-71 <naval@ucreate.co.in>
2019-02-28 17:49:40 +0530

    theme

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,29 +1,26 @@
-  <!-- Required meta tags-->
-  <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="au theme template">
-    <meta name="author" content="Hau Nguyen">
-    <meta name="keywords" content="au theme template">
-    <!-- Title Page-->
-    <title>@yield('pageTitle') - Trello Control Cecklist</title>
-    <!-- Fontfaces CSS-->
-    <link href="{{ URL::asset('cooladmin/css/font-face.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
+<!-- META SECTION -->
+<title>@yield('pageTitle') - Trello Control Cecklist</title>          
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS-->
-    <link href="{{ URL::asset('cooladmin/vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
+<link rel="icon" href="favicon.ico" type="image/x-icon" />
+<!-- END META SECTION -->
 
-    <!-- Vendor CSS-->
-    <link href="{{ URL::asset('cooladmin/vendor/animsition/animsition.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/wow/animate.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/slick/slick.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/select2/select2.min.css') }}" rel="stylesheet" media="all">
-    <link href="{{ URL::asset('cooladmin/vendor/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" media="all">
+<!-- CSS INCLUDE -->        
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/theme-default.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/jquery/jquery-ui.min.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/bootstrap/bootstrap.min.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/fontawesome/font-awesome.min.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/summernote/summernote.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/codemirror/codemirror.css') }}"/>
 
-    <!-- Main CSS-->
-    <link href="{{ URL::asset('cooladmin/css/theme.css') }}" rel="stylesheet" media="all">
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/nvd3/nv.d3.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/mcustomscrollbar/jquery.mCustomScrollbar.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/fullcalendar/fullcalendar.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/blueimp/blueimp-gallery.min.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/rickshaw/rickshaw.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/dropzone/dropzone.css') }}"/>
+<link rel="stylesheet" type="text/css" id="theme" href="{{ URL::asset('jolly/css/animate/animate.min.css') }}"/>
+<!-- EOF CSS INCLUDE -->      
 
