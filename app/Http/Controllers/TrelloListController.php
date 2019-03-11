a513cfbebc57747cdf982eb5921d0521e7967bb8
naval-ucreate <naval@ucreate.co.in>
2019-03-11 15:25:02 +0530

    add entry in db when hook called

--- app/Http/Controllers/TrelloListController.php
+++ app/Http/Controllers/TrelloListController.php
@@ -3,19 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
-use Trello\Client;
-use Trello\Service;
-use Trello\Events;
+
+use App\Models\WebhookCallLog;
 
 class TrelloListController extends Controller
 {
-    //
-
-
-
-
-
+   
     public function postGuzzleRequest(){
 
         $token                  = Session::get('userinfo')['token'];
@@ -27,41 +20,14 @@ class TrelloListController extends Controller
         $myBody['callbackURL']  = "http://trellocontrollchecklist.herokuapp.com/test-web-hook";
         $myBody['idModel']      = "5c6bb49e2b175466e1f763a1";
         $myBody['description']  = "My First Trello App";
-        $request                = $client->post($url,$headers,['form_params'=>$myBody]);
+        $request                = $client->post($url,['form_params'=>$myBody]);
         $response               = $request->send();
         dd($response);
     }
+    public function testWebHook(WebhookCallLog $webhook_calllog){  
 
-    function toWriteInFile($board){
-        $myfile = fopen( getcwd()."newfile.txt", "w") or die("Unable to open file!");
-        $txt    = $board;
-        fwrite($myfile, $txt);
-        $txt    = "Jane Doe\n";
-        fwrite($myfile, $txt);
-        fclose($myfile);
-    }
-
-    public function testWebHook(){  
-        
-        $token  =  Session::get('userinfo')['token'];
-        $api    = '5b60d3f32d9fadef119dfaf96af008ba';
-        $client = new Client();
-        $client->authenticate($api,$client, Client::AUTH_URL_CLIENT_ID);
-
-        $service = new Service($client);
-
-
-
-        dd("test");
-
-        // Bind a callable to a given event...
-        $service->addListener(Events::BOARD_UPDATE, function ($event) {
-        $board = $event->getBoard();
-
-        print_R($board);
-        $this->toWriteInFile($board);   
-        // do something
-        });
-    }
-
+        $data =   ['body'=>'testing'];
+        $webhook_calllog->create($data);
+    }   
+   
 }
