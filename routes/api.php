547de5ebb897916f12d8a69d3def96008426009b
naval-ucreate <naval@ucreate.co.in>
2019-03-11 18:32:25 +0530

    update route error

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/test-web-hook','TrelloListController@testWebHook')->name('test_web_hook');
+Route::any('/test-web-hook','TrelloListController@testWebHook')->name('test_web_hook');
