fb6dac579fe762a970c10b5590fcab03f42ce19b
pankaj <pankaj@ucreate.co.in>
2019-03-12 19:18:46 +0530

    fixed the error

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -11,7 +11,7 @@ class ListController extends Controller
 
     public function store(array $data,$id){
         if(BoardList::insert($data)){
-            return BoardList::where('trello_board_id','=',$id)->get()->toArray();;  
+            return BoardList::with('board')->where('trello_board_id','=',$id)->get()->toArray();  
         }
     }
 
@@ -26,14 +26,14 @@ class ListController extends Controller
     public function TrelloList(String $id){       
         //$board_list          =   BoardList::where('trello_board_id','=',$id)->get()->toArray();
 
-        $board_list          =   BoardList::with('board')->get()->toArray();
+        $board_list          =   BoardList::with('board')->where('trello_board_id','=',$id)->get()->toArray();
         if(!count($board_list)){
             $list_data      =   app('trello')->GetBoardList($id);
             //dd($list_data);
             if(count($list_data)){
                 foreach($list_data['lists'] as $list_val){
                     $insert_data[] = [
-                    'trello_board_id'=>$list_val['idBoard'],
+                    'trello_board_id'=>$id,
                     'trello_list_id'=>$list_val['id'],
                     'name'=> $list_val['name'],
                     'web_hook_enable'=>0,
