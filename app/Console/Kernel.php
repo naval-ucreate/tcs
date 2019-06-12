fc75448b831accff07b96c3d3450732d63769330
pankaj <pankaj@ucreate.co.in>
2019-06-12 15:19:34 +0530

    change the schedule daily to week

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('create:PerformanceCard')->weeklyOn(5, '10:00');
+        $schedule->command('create:PerformanceCard')->weeklyOn(5);
         //$schedule->command('create:PerformanceCard')->everyFiveMinutes();
     }
 
